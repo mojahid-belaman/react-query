6b32153afd208ce13afc128a001d441097847971
mojahid-belaman <mojahidbelaman@gmail.com>
2023-09-21 14:56:30 +0100

    update UI and use hook useMutation to save data in server

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,41 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { createEvent } from "../../utils/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createEvent,
+  });
+  function handleSubmit(formData) {
+    mutate(formData);
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
         <>
           <Link to="../" className="button-text">
             Cancel
           </Link>
-          <button type="submit" className="button">
-            Create
+          <button type="submit" className="button" disabled={isPending}>
+            {isPending ? "Submitting..." : "Create"}
           </button>
         </>
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title={"an error occured"}
+          message={
+            error.info?.message || "an error occured when creating event."
+          }
+        />
+      )}
     </Modal>
   );
 }
